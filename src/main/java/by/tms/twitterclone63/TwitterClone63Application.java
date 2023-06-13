ef009d99836342fbf065fe0e218aa475c79554ab
Simon Pirko <simonpirko@Simons-MacBook-Pro.local>
2023-06-13 19:08:38 +0300

    Initial commit

---
+++ src/main/java/by/tms/twitterclone63/TwitterClone63Application.java
@@ -0,0 +1,13 @@
+package by.tms.twitterclone63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterClone63Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TwitterClone63Application.class, args);
+  }
+
+}
---
+++ src/test/java/by/tms/twitterclone63/TwitterClone63ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.twitterclone63;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterClone63ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
